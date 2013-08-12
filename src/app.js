9ab7b356e4e62936047c40135616b3d384b17a7b
Mirko Kiefer <mail@mirkokiefer.com>
2013-08-12 18:07:04 +0200

    display static todos

--- src/app.js
+++ src/app.js
@@ -1,2 +1,54 @@
 
-console.log(1)
+var initialTodos = [
+  {title: 'Go to Node.js Meetup', isDone: true},
+  {title: 'Learn to node', isDone: false},
+  {title: 'Write a cool app', isDone: false}
+]
+
+var model = {}
+model.Todo = Backbone.Model.extend({})
+
+model.Todos = Backbone.Collection.extend({
+  model: model.Todo
+})
+
+var allTodos = new model.Todos(initialTodos)
+
+var views = {}
+
+views.TodoItem = Backbone.View.extend({
+  initialize: function() {
+    this.listenTo(this.model, "change", this.render)
+    var source   = $("#todo-template").html()
+    this.template = Handlebars.compile(source)
+  },
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()))
+    return this
+  }
+})
+
+views.Todos = Backbone.View.extend({
+  initialize: function() {
+    this.listenTo(this.collection, "change", this.render)
+    var source   = $("#todos-template").html()
+    this.template = Handlebars.compile(source)
+  },
+  render: function() {
+    this.$el.html(this.template())
+    var childViews = this.collection.map(function(each) {
+      return new views.TodoItem({model: each})
+    })
+
+    var el = this.$('.todo-list')
+    el.empty()
+    childViews.forEach(function(each) {
+      el.append(each.render().el)
+    })
+    return this
+  }
+})
+
+var todosView = new views.Todos({collection: allTodos})
+
+$('#todos').html(todosView.render().el)
